52d63bb18174103d820b6afd7d03ece994721245
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-24 22:24:34 +0900

    user custom part 4

--- house/models.py
+++ house/models.py
@@ -4,10 +4,18 @@ from django.db import models
 # Create your models here.
 class House(models.Model):
     name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Postive Number Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=128)
-    pet_allowed = models.BooleanField(default=False)
+    pet_allowed = models.BooleanField(
+        default=False,
+        verbose_name="Pets Allowed?",
+        help_text="This house allows pets?"
+    )
+    # unique key not a just simple integer
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
